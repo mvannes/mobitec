442cffe91e63836c37dd77736b8ba738775c322b
Michael van Nes <m.f.van.nes@gmail.com>
2024-10-24 13:44:32 +0200

    Split into packages for server prep

---
+++ serial-port/serial-port.go
@@ -0,0 +1,29 @@
+package serial_port
+
+import (
+	"errors"
+	"fmt"
+	"go.bug.st/serial"
+	"log"
+)
+
+func GetPort() (serial.Port, error) {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		return nil, errors.New("no serial ports found")
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 4800,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	return serial.Open(ports[0], mode)
+}
