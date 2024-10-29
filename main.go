3b8dfec93adf74fd41ef80d7f82b983d0937309b
Michael van Nes <m.f.van.nes@gmail.com>
2024-10-29 21:58:47 +0100

    Add cobra commands, make cmd to set text with optional serial port disabled for testing purposes

--- main.go
+++ main.go
@@ -2,38 +2,13 @@ package main
 
 import (
 	"log"
-	"mobitec/flipdot"
-	"mobitec/serialport"
-	"time"
+	"mobitec/cmd"
 )
 
 func main() {
-	port, err := serialport.GetPort()
+
+	err := cmd.Execute()
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer port.Close()
-
-	f := flipdot.NewFlipdot(112, 19, 0x07, port)
-
-	messages := []string{"SISYPHOS", "PYTHIOS", "PYTHSYPHOS"}
-	currentMsg := 0
-	for {
-		currentMsg++
-		if currentMsg > 2 {
-			currentMsg = 0
-		}
-		m := flipdot.Message{
-			Text:             messages[currentMsg],
-			Font:             "text_13px_bold",
-			HorizontalOffset: 9,
-			VerticalOffset:   19,
-		}
-
-		err := f.Send(m)
-		if err != nil {
-			log.Fatal(err)
-		}
-		time.Sleep(time.Second * 60)
 	}
 }
