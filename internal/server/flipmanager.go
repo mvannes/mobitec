3a18cc23afa6fdd859149b60b6ad61c131f154cc
Michael van Nes <m.f.van.nes@gmail.com>
2024-11-14 17:35:17 +0100

    Add simple ui using go embed

--- internal/server/flipmanager.go
+++ internal/server/flipmanager.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"mobitec/internal/flipdot"
 	"time"
@@ -20,9 +19,11 @@ func manageFlipdot(f *flipdot.Flipdot, msgChan chan flipdot.Message) {
 }
 
 func handleMsg(f *flipdot.Flipdot, msg flipdot.Message) {
-	fmt.Println("Handling msg!")
 	err := f.Send(msg)
 	if err != nil {
+		// TODO: create some form of err handling that is either better logging or
+		// TODO: cancelling of the server as it is.
+		// TODO: Either way, from context of a go routine there is little to do here right now.
 		log.Println("error occurred during msg sending: ", err)
 		return
 	}
