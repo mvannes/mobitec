ec4a2ff38ba3f8ff38c8e4372cfda9a59f4b829e
Michael van Nes <m.f.van.nes@gmail.com>
2024-11-14 17:35:17 +0100

    Add simple ui using go embed

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,15 +1,19 @@
 package server
 
 import (
+	_ "embed"
 	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"io"
 	"log"
 	"mobitec/internal/flipdot"
+	"mobitec/internal/serialport"
 	"net/http"
 )
+
+//go:embed assets/index.html
+var uiFile string
 
 func Start() error {
 	r := chi.NewRouter()
@@ -21,14 +25,14 @@ func Start() error {
 	r.Use(cors.Handler(cors.Options{AllowedOrigins: []string{"*"}}))
 
 	//var err error
-	// TODO: Configure option to start with this serialport configured instead of commenting out.
-	// TODO: @see text.go flag handling.
-	//port, err := serialport.GetPort()
-	//if err != nil {
-	//	return err
-	//}
+	//TODO: Configure option to start with this serialport configured instead of commenting out.
+	//TODO: @see text.go flag handling.
+	port, err := serialport.GetPort()
+	if err != nil {
+		return err
+	}
 
-	port := io.Discard
+	//port := io.Discard
 
 	// TODO: make configurable.
 	f := flipdot.NewFlipdot(112, 19, 0x07, port)
@@ -36,7 +40,10 @@ func Start() error {
 	// Make this smarter, because now the queue is maxed at 200, which seems like a lot.
 	msgChan := make(chan flipdot.Message, 50)
 	r.Mount("/flipdot", newControlRouter(msgChan))
-
+	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(200)
+		writer.Write([]byte(uiFile))
+	})
 	go manageFlipdot(f, msgChan)
 
 	return http.ListenAndServe(":8080", r)
